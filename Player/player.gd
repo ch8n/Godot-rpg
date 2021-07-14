16e7f773c6724687bd53034f9a3214ab7f8d00a3
ch8n <chetan.garg36@gmail.com>
2021-07-15 04:03:20 +0530

    :construction: added velocity and acceleration

--- Player/player.gd
+++ Player/player.gd
@@ -1,12 +1,24 @@
 extends KinematicBody2D
 
-
+const ACCELERATION_RATE = 10
+const MAX_PLAYER_SPEED = 100
+const FRICTION_RATE = 2.5
 
 func _ready():
 	print("Hello godot!")
 
+var velocity = Vector2.ZERO
+
 func _physics_process(delta):
-	var velocity = Vector2.ZERO
-	velocity.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	velocity.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	var direction = Vector2.ZERO
+	direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	direction.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	direction = direction.normalized()
+
+	if direction != Vector2.ZERO :
+		velocity += direction * ACCELERATION_RATE * delta
+		velocity = velocity.clamped(MAX_PLAYER_SPEED * delta)
+	else :
+		velocity = velocity.move_toward(Vector2.ZERO,FRICTION_RATE * delta)
+	print(velocity)
 	move_and_collide(velocity)
